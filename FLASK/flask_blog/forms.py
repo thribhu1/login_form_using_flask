e5835ff199d5e695972a738634d2c749f609769e
thribhuvan1 <35718176+thribhu1@users.noreply.github.com>
2021-07-28 09:58:46 -0400

    commit on login_auth

--- FLASK/flask_blog/forms.py
+++ FLASK/flask_blog/forms.py
@@ -1,7 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import DataRequired, Length, Email, EqualTo,ValidationError
 from flask_blog.models import User
+
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -11,7 +14,7 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Update')
 
     def validate_username(self,username):
         user=User.query.filter_by(username=username.data).first()
@@ -19,8 +22,9 @@ class RegistrationForm(FlaskForm):
             raise ValidationError('that user name is taken, please chose another oner')
 
     def validate_email(self,email):
-        email=User.query.filter_by(email=email.data).first()
-        if email:
+        
+        user=User.query.filter_by(email=email.data).first()
+        if user:
             raise ValidationError('that email is taken, please chose another oner')
 
 
@@ -30,3 +34,26 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg','png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self,username):
+        if username.data != current_user.username:
+            user=User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('that user name is taken, please chose another oner')
+
+    def validate_email(self,email):
+        if email.data != current_user.email:
+            email=User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('that email is taken, please chose another oner')
--- FLASK/flask_blog/routes.py
+++ FLASK/flask_blog/routes.py
@@ -4,8 +4,11 @@
 #config values on application
 from flask import render_template, url_for, flash, redirect,request
 #decrators are using 
+import os
+import secrets
+from PIL import Image
 from flask_blog import app,db,bcrypt
-from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.forms import RegistrationForm, LoginForm,UpdateAccountForm
 from flask_blog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -46,7 +49,7 @@ def register():
         User1=User(username=form.username.data,email=form.email.data,password=hashed_pwd)
         db.session.add(User1)
         db.session.commit()
-
+ 
         flash(f'your account has been created! you are able to login', 'success')
         return redirect(url_for('login'))
     return render_template('register.html',title='Register', form=form)
@@ -64,7 +67,7 @@ def login():
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
 
-            return redirect(nextpage) if nextpage else redirect(url_for('home'))
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login',  form=form)
@@ -75,8 +78,41 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-            
-@app.route("/account")
+def save_picture(form_picture):
+    random_hex=secrets.token_hex(8)
+    _,f_ext=os.path.splitext(form_picture.filename)#one commonthing pythonist do is throw away the f_name
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path,'static/profile_pics',picture_fn)
+    form_picture.save(picture_path)
+
+    output_size = (125,125)
+    i =Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+    #root_path attribute give the rooth_path of the applicatin 
+
+
+@app.route("/account",methods=['POST','GET'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form=UpdateAccountForm()
+    if form.validate_on_submit():
+
+        if form.picture.data:
+            picture_file=save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username=form.username.data
+        current_user.email=form.email.data
+        db.session.commit()
+        flash('your account has been updatae !', 'success')
+        return redirect(url_for('account'))  #(post->get->redirect pattern) #are you sure you want to reload
+          #  this is because it will run post request<<< - #  msg comes      your data will be submited are you sure to reload
+        #it will relodad post method when we try to submit
+    elif request.method == 'GET':  # here we are adding for redirect after the data is updated
+        form.username.data="Got you"
+        form.email.data=current_user.email
+
+    image_file = url_for('static',filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file,form=form)
